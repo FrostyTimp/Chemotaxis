e96f093619225ca2794f478100d88f0244a147b0
unknown <anxu4@697-334-Aragorn.sf.k12.internal>
2015-09-18 07:53:35 -0700

    Removed food

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -15,14 +15,10 @@ import java.io.IOException;
 public class Chemotaxis extends PApplet {
 
 Bacteria [] dot;
-Food feed;
  public void setup()   
  {     
  	size(800,800);
- 	stroke(0);
- 	fill(0);
- 	ellipse(0,0,30,30);
- 	dot=new Bacteria[250];
+ 	dot=new Bacteria[100];
  	for(int i=0;i<dot.length;i++)
  	{
  		dot[i]=new Bacteria((int)(Math.random()*780)+10,(int)(Math.random()*780)+10);
@@ -37,34 +33,7 @@ Food feed;
  		dot[i].walk();
  		dot[i].show();
  	}
- 	if(mousePressed==true)
- 	{
- 		feed=new Food(mouseX,mouseY);
- 		feed.show();
- 	}
- 	// feed.show();
  }  
- // void mouseClicked()
- // {
- // 	feed=new Food(mouseX,mouseY);
- // 	feed.show();
- // }
- class Food
- {
- 	int foodX,foodY;
- 	Food(int placeX, int placeY)
- 	{
- 		show();
- 		foodX=placeX;
- 		foodY=placeY;
- 	}
- 	public void show()
- 	{
- 		stroke(255);
- 		fill(255);
- 		ellipse(foodX,foodY,30,30);
- 	}
- }
  class Bacteria    
  {     
  	int myX,myY,r,g,b;
